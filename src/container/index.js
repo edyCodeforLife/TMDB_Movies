c23a3b4636f112dc274ba35882e1168075e7d3a1
Edy Susanto <edy.susanto@moduit.id>
2020-10-09 14:36:20 +0700

    1. Initial commit

---
+++ src/container/index.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useCallback, useState, useRef } from "react";
+import "../components/app.scss";
+import axios from "axios";
+
+function _HomePage(props) {
+	return (
+		<div>
+			dsadsadsadssa
+		</div>
+	)
+}
+export const HomePage = React.memo((_HomePage));
